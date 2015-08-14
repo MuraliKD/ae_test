c1cacc4f175158ca769206b2562a47d23d121a7a
Prelang Builder <builder@prelang.com>
2015-08-14 20:15:20 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'BusinessStrategy' to allow for create, update

--- app/admin/business_strategy.rb
+++ app/admin/business_strategy.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register BusinessStrategy do
+  permit_params :vision, :principles, :business_model, :form_factor, :pricing, :sales
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
