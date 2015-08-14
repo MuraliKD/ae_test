bcbd2523fa3a20574d5cb3ffeeccf4589ac8974d
Prelang Builder <builder@prelang.com>
2015-08-14 20:15:26 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'User' to allow for create, update

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register User do
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :name, :startup_name, :startup_description, :product_name, :product_description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
