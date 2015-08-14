45a52817af63cfe8ec5a1af963046036bff4bbba
Prelang Builder <builder@prelang.com>
2015-08-14 20:15:14 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/business_strategies_controller.rb
+++ app/controllers/business_strategies_controller.rb
@@ -1,4 +1,8 @@
 class BusinessStrategiesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_business_strategy, only: [:show, :edit, :update, :destroy]
 
   # GET /business_strategies
